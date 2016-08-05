09c13541a371cd538ecc56cd09df569d370071a1
iotmaker <achasma@gmail.com>
2016-08-05 17:52:54 +1000

    Add files via upload

---
+++ 02_CodeFiles/ELearningActivity1/week01_02/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = " Haesung Lee "
+        TextBox2.Text = " S078887 "
+        TextBox3.Text = " 2/10 Orwell St, Potts Poin, NSW, 2011, Australia"
+        TextBox4.Text = " 0432-399-841 "
+        DateTimePicker1.Value.DayOfYear.ToString()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
