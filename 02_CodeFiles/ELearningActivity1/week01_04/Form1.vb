404a1c077e2b8f4def463b65b0ad8e354fa358c2
iotmaker <achasma@gmail.com>
2016-08-08 16:15:17 +1000

    Add files via upload

---
+++ 02_CodeFiles/ELearningActivity1/week01_04/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim FahrenheitValue, CelsiusValue As Double
+
+        CelsiusValue = Val(celsiusBox.Text) ' Input variable
+
+        FahrenheitValue = ((9 / 5) * CelsiusValue) + 32  ' Conversion from C to F
+
+        fahrenheitBox.Text = FahrenheitValue ' Output variable
+
+    End Sub
+End Class
