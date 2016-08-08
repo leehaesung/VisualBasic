5970f12cb883f0c33b455bd0dfc022cad8bb3e9b
iotmaker <achasma@gmail.com>
2016-08-08 16:07:52 +1000

    Add files via upload

---
+++ 02_CodeFiles/ELearningActivity2/week02_01/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_compare.Click
+        ' variabls
+        Dim fno, sno As Double
+        fno = txt_fno.Text
+        sno = txt_sno.Text
+
+        ' Comparing between fnd and sno.
+        If (fno > sno) Then
+            MsgBox("1st number is bigger than 2nd number.", fno, sno)
+        ElseIf (fno < sno) Then
+            MsgBox("1st number is smaller 2nd number.", fno, sno)
+        Else
+            MsgBox("1st number is same as 2nd number.", fno, sno)
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        txt_fno.Text = "" 'clear 1st number text box.'
+        txt_sno.Text = "" 'clear 2nd number text box.
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Close()  'Exit 
+    End Sub
+End Class
